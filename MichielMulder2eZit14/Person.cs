b199cdedf312838ff2e2f9b848b9dabc1dd4badd
Muldoser <michielmldr@gmail.com>
2015-09-02 09:44:06 +0200

    gitignore verbeterd. (".gitignore") Basic layout xaml. Json paste special to Person.cs class. Json.net installed via nuget. Exercise 2 completed, profiles listed according to requirements.

---
+++ MichielMulder2eZit14/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MichielMulder2eZit14
+{
+
+    public class Rootobject
+    {
+        public Personen[] Personen { get; set; }
+    }
+
+    public class Personen : INotifyCollectionChanged
+    {
+        public string _id { get; set; }
+        public int index { get; set; }
+        public bool isActive { get; set; }
+        public string balance { get; set; }
+        public string mugshot { get; set; }
+        public int age { get; set; }
+        public string eyeColor { get; set; }
+        public string fullname { get; set; }
+        public string gender { get; set; }
+        public string company { get; set; }
+        public string email { get; set; }
+        public string telnumber { get; set; }
+        public string address { get; set; }
+        public string about { get; set; }
+        public string registered { get; set; }
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public string[] tags { get; set; }
+        public Friend[] friends { get; set; }
+        public string greeting { get; set; }
+        public string favoriteFruit { get; set; }
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+    }
+
+    public class Friend
+    {
+        public int id { get; set; }
+        public string fullname { get; set; }
+    }
+
+}
